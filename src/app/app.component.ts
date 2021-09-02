e5cc9f825cc4ef79747e915b69e783a137fba71e
Dalmasio <dalmasiof@gmail.com>
2021-09-01 21:13:23 -0300

    add Functions to borderRaid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'borderRadiusApp';
+  subs?: Subscription
+
+  mousemove = fromEvent(document, 'mousemove');
+  mouseup = fromEvent(document, 'mouseup');
+
+  lastPosition  : number = 0
+
+
+  ngOnInit(): void {
+
+
+
+  }
+
+  onHoverClick(position: string, multiplier: number) {
+    debugger
+    var element = document.getElementById("innerBox");
+
+    if (element) {
+
+
+
+      switch (position) {
+
+        case "Tl":
+          let tl = 0
+          if(element.style.borderTopLeftRadius.replace('%', '')!="")
+           tl = parseInt(element.style.borderTopLeftRadius.replace('%', ''))
+
+           tl += multiplier
+
+          element.style.borderTopLeftRadius = tl + "%"
+          break;
+
+        case "Tr":
+          let tr = 0
+          if(element.style.borderTopRightRadius.replace('%', '')!="")
+           tr = parseInt(element.style.borderTopRightRadius.replace('%', ''))
+
+           tr += multiplier
+
+          element.style.borderTopRightRadius = tr + "%"
+
+          break;
+
+        case "Br":
+          let br = 0
+          if(element.style.borderBottomRightRadius.replace('%', '')!="")
+           br = parseInt(element.style.borderBottomRightRadius.replace('%', ''))
+
+           br += multiplier
+
+         element.style.borderBottomRightRadius = br + "%"
+          break;
+
+        case "Bl":
+          let bl = 0
+          if(element.style.borderBottomLeftRadius.replace('%', '')!="")
+           bl = parseInt(element.style.borderBottomLeftRadius.replace('%', ''))
+
+           bl += multiplier
+
+         element.style.borderBottomLeftRadius = bl + "%"
+          break;
+      }
+    }
+
+
+
+  }
+
+  changeRadius() {
+
+  }
+
+
+  pointDown(position: string) {
+    this.mousemove.pipe(
+      takeUntil(this.mouseup)
+    ).subscribe((em: MouseEventInit) => {
+      debugger
+      let x = em.screenX
+      if (x) {
+        if (this.lastPosition < x) {
+          this.onHoverClick(position, -1);
+          this.lastPosition = x;
+        }
+        else {
+          this.onHoverClick(position, 1);
+          this.lastPosition = x;
+        }
+      }
+
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
