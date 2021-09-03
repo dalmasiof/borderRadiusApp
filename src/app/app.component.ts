4ad0bea29a97c3c6174767ea9db991f2e3d646d2
Dalmasio <dalmasiof@gmail.com>
2021-09-02 23:01:36 -0300

    startInputsvalues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,18 @@ import { takeUntil } from 'rxjs/operators';
 export class AppComponent implements OnInit {
 
   title = 'borderRadiusApp';
-  subs?: Subscription
+  subs?: Subscription;
+
 
   mousemove = fromEvent(document, 'mousemove');
   mouseup = fromEvent(document, 'mouseup');
 
+  innerBoxBorderRadius?: string
+
   ngOnInit(): void {
   }
 
-  onHoverClick(position: string, multiplier: number) {
-    debugger
+  applyRadius(position: string, multiplier: number) {
     var element = document.getElementById("innerBox");
 
     if (element) {
@@ -68,6 +70,73 @@ export class AppComponent implements OnInit {
           break;
       }
     }
+    this.generateBorderRadiusString()
+
+  }
+
+  appyRadiusFromInput(position: string, valueInput: string) {
+debugger
+    (valueInput == "" ? valueInput = "0" : valueInput = valueInput)
+    var multiplier = parseInt(valueInput)
+    var element = document.getElementById("innerBox");
+
+    if (element) {
+      switch (position) {
+
+        case "Tl":
+          let tl = 0
+          if (element.style.borderTopLeftRadius.replace('%', '') != "")
+            tl = parseInt(element.style.borderTopLeftRadius.replace('%', ''))
+
+          tl = multiplier
+
+          element.style.borderTopLeftRadius = tl + "%"
+          break;
+
+        case "Tr":
+          let tr = 0
+          if (element.style.borderTopRightRadius.replace('%', '') != "")
+            tr = parseInt(element.style.borderTopRightRadius.replace('%', ''))
+
+          tr = multiplier
+
+          element.style.borderTopRightRadius = tr + "%"
+
+          break;
+
+        case "Br":
+          let br = 0
+          if (element.style.borderBottomRightRadius.replace('%', '') != "")
+            br = parseInt(element.style.borderBottomRightRadius.replace('%', ''))
+
+          br = multiplier
+
+          element.style.borderBottomRightRadius = br + "%"
+          break;
+
+        case "Bl":
+          let bl = 0
+          if (element.style.borderBottomLeftRadius.replace('%', '') != "")
+            bl = parseInt(element.style.borderBottomLeftRadius.replace('%', ''))
+
+          bl = multiplier
+
+          element.style.borderBottomLeftRadius = bl + "%"
+          break;
+      }
+    }
+    this.generateBorderRadiusString()
+  }
+
+  generateBorderRadiusString() {
+    debugger
+    var element = document.getElementById("innerBox");
+    if (element) {
+      this.innerBoxBorderRadius = `${element.style.borderTopLeftRadius},
+      ${element.style.borderTopRightRadius},
+      ${element.style.borderBottomRightRadius},
+      ${element.style.borderBottomLeftRadius},`
+    }
   }
 
   pointDown(position: string) {
@@ -76,9 +145,8 @@ export class AppComponent implements OnInit {
     this.mousemove.pipe(
       takeUntil(this.mouseup)
     ).subscribe((em: MouseEventInit) => {
-      debugger
       let x = em.movementX
-      let valueOfRadius = 1//values do define if the border radsu will increase or decrease
+      let valueOfRadius = 1//values do define if the border radius will increase or decrease
 
       if (x) {
         if (direction == 'r') {
@@ -91,7 +159,7 @@ export class AppComponent implements OnInit {
             valueOfRadius = -1
           }
         }
-        this.onHoverClick(position, valueOfRadius);
+        this.applyRadius(position, valueOfRadius);
       }
     })
   }
